07a02161e6927b8ff806a951b4503c32f62b3f7a
kejacobsen <katiej27@uw.edu>
2024-11-07 21:02:02 -0700

    add templates, models, views

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "polling"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "django_blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "django_blog/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
