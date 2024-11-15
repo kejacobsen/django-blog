ae57f057d1773d36f0a50ef0ddb3c31ec6dd0374
kejacobsen <katiej27@uw.edu>
2024-11-15 15:26:57 -0700

    add comments

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 
-# InlineModelAdmin: represents Categories on the Post Admin Page
+
+"""
+InlineModelAdmin: represents Categories on the Post Admin Page
+
+reference: https://docs.djangoproject.com/en/5.1/ref/contrib/admin/#working-with-a-model-with-two-or-more-foreign-keys-to-the-same-parent-model
+The through attribute is a reference to the model that manages
+the many-to-many relation. This model is automatically created
+by Django when you define a many-to-many field.
+"""
+
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
 
