69d0f53536433fdf6f95cf460f41c3768d98f276
kejacobsen <katiej27@uw.edu>
2024-11-15 15:17:45 -0700

    only add categories to posts

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+# InlineModelAdmin: represents Categories on the Post Admin Page
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ['posts']
