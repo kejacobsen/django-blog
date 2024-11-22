227ecab895e1f0fcff8e84bc1c3f1711aa13f0f8
kejacobsen <katiej27@uw.edu>
2024-11-21 17:21:44 -0700

    convert function-based views into class-based views

--- blogging/views.py
+++ blogging/views.py
@@ -1,33 +1,14 @@
-from django.http import Http404
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from blogging.models import Post
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args: \n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Keyword args: \n"
-        body += "\n".join(["\t%s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by("-published_date")
 
-    # for debugging
-    print(f"Found {posts.count()} published posts.")  # This will show if the query returns the expected posts
-
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+    queryset = Post.objects.exclude(published_date__exact=None)
