2df012f68a21f8314afe4601217c447e50cec532
kejacobsen <katiej27@uw.edu>
2024-12-09 13:48:03 -0700

    typo

--- django_blog/heroku.py
+++ django_blog/heroku.py
@@ -1,11 +1,10 @@
 import os
 import dj_database_url
-
 from .settings import *
 
 DATABASES = {
     "default": dj_database_url.config(
-        default='sqlite:///os.path.join(BASE_DIR, "db.sqlite3"))'
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
     )
 }
 
@@ -15,4 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
 
-MIDDLEWARE = ("whitenoise.middleware.WhiteNoiseMiddleware" * MIDDLEWARE,)
+MIDDLEWARE = (
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    *MIDDLEWARE,
+)
